e21a62cbc850fbd8983f8c887a762c4c84d3708d
Ariana Rexhepi <arianaarexhepi@gmail.com>
2024-04-01 15:18:41 +0200

    database connection

--- back/Models/Book.cs
+++ back/Models/Book.cs
@@ -7,14 +7,14 @@ namespace back.Models
 {
     public class Book
     {
-        public int Id { get; set; }
-        public string Title { get; set; }
-        public string Author { get; set; }
-        public string Description { get; set; }
-        public string Category { get; set; }
+        public Guid Id { get; set; }
+        public string ?Title { get; set; }
+        public string ?Author { get; set; }
+        public string ?Description { get; set; }
+        public string ?Category { get; set; }
         public int Rating { get; set; }
         public DateTime Year { get; set; }
-        public string Image { get; set; }
+        public string ?Image { get; set; }
         public int Price { get; set; }
     }
 }
