1ad9f6bc5f1a2619724224fafaea3aa175b253b6
Ariana Rexhepi <arianaarexhepi@gmail.com>
2024-04-02 12:53:03 +0200

    migrations added

--- back/Data/AppDbContext.cs
+++ back/Data/AppDbContext.cs
@@ -1,20 +1,14 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using back.Models;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using back.Models;
 
 namespace back.Data
 {
-    public class AppDbContext : IdentityDbContext<AppUser>
+    public class AppDbContext : DbContext
     {
-        public AppDbContext(DbContextOptions options) : base(options)
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
         }
 
         public DbSet<Book> Books { get; set; }
-
     }
 }
