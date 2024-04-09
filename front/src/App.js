08ad2a268add3edecb84ca14a68943353461e58f
Ariana Rexhepi <arianaarexhepi@gmail.com>
2024-04-09 20:15:25 +0200

    navbar creation

--- front/src/App.js
+++ front/src/App.js
@@ -1,12 +1,16 @@
 import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
 import Pages from './components/Pages';
+import Nav from './components/Nav';
 
 function App() {
   return (
     <Router>
     <div className="App">
+      <>
+      <Nav/>
       <Pages/>
+      </>
     </div>
     </Router>
   );
