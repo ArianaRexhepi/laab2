e783e11daa1b40379db661ad5941ff28d1ee37a6
Ariana Rexhepi <arianaarexhepi@gmail.com>
2024-04-13 10:00:26 +0200

    .

--- back/obj/Debug/net6.0/back.AssemblyInfo.cs
+++ back/obj/Debug/net6.0/back.AssemblyInfo.cs
@@ -13,11 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("back")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-<<<<<<< Updated upstream
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59988f60cacf566112bdb911119027e074a2fe88")]
-=======
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9e3ae8665865e1fc0471ff3df28d5315bae03e6")]
->>>>>>> Stashed changes
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("back")]
 [assembly: System.Reflection.AssemblyTitleAttribute("back")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
