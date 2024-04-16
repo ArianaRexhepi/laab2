a2c96fe200b54274315e1662dd6701eaf979af04
Blin <bk51836@ubt-uni.net>
2024-04-16 23:22:55 +0200

    CREATION OF A NEW MODEL

---
+++ back/Models/Review.cs
@@ -0,0 +1,17 @@
+namespace back.Models
+{
+    public class Review
+{
+    [Required]
+    public int Guid Id { get; set; }
+    [Required]
+    public int BookId { get; set; }
+    [Required]
+    public string UserName { get; set; }
+    [Required]
+    public string Content { get; set; }
+    [Required]
+    public int Rating { get; set; }
+}
+
+}
